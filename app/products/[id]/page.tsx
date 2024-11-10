e6b1595f03e615a0ff191762ee05ac82a3a752f6
Tahasiraj1 <tahasiraj242@gmail.com>
2024-11-10 08:11:13 +0500

    eleven

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -7,6 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Heart } from 'lucide-react';
 import { TiStar } from "react-icons/ti";
 import { RiShoppingCart2Line } from "react-icons/ri";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 
 const ProductDetails = () => {
     const params = useParams();
@@ -16,6 +17,7 @@ const ProductDetails = () => {
     if (!product) return <p className='h-screen items-center flex justify-center font-bold text-2xl text-red-500'>Product not found</p>;
 
     return(
+        <>
         <div className='flex lg:flex-row sm:flex-col py-20 px-5'>
             <div className='grid grid-cols-2 gap-1'>
             <Image
@@ -33,7 +35,7 @@ const ProductDetails = () => {
                 className='w-[400px] h-[500px] object-cover'
             />
             </div>
-            <div className='flex flex-col pl-20 pr-5 w-full max-w-2xl'>
+            <div className='flex flex-col lg:pl-20 pr-5 w-full max-w-2xl'>
                 <h1 className='text-5xl mb-9 mt-5 font-bold'>
                     {product.name}
                 </h1>
@@ -50,7 +52,7 @@ const ProductDetails = () => {
                     <label>Colors:</label>
                     {product.colors.map((color) => (
                         <Button
-                        className='border rounded-none bg-transparent hover:bg-gray-200 text-black border-gray-500 hover:border-gray-800'
+                        className='border rounded-none bg-lime-100 hover:bg-gray-200 text-black border-gray-500 hover:border-gray-800'
                         >
                             {color}
                         </Button>
@@ -60,7 +62,7 @@ const ProductDetails = () => {
                     <label>Sizes:</label>
                     {product.sizes.map((size) => (
                         <Button
-                        className='border rounded-none bg-transparent hover:bg-gray-200 text-black border-gray-500 hover:border-gray-800'
+                        className='border rounded-none bg-lime-100 hover:bg-gray-200 text-black border-gray-500 hover:border-gray-800'
                         >
                             {size}
                         </Button>
@@ -76,13 +78,46 @@ const ProductDetails = () => {
                         ADD TO CART
                     </Button>
                     <Button
-                    className='w-18 rounded-none border border-emerald-600 bg-lime-100 hover:bg text-black hover:text-white'
+                    className='w-18 rounded-none border border-emerald-600 bg-lime-100 hover:bg-gray-200 text-black'
                     >
                         <Heart />
                     </Button>
                 </div>
             </div>
-    </div>
+        </div>
+        <h1 className='py-5 text-4xl font-bold sm:px-12 lg:px-40'>
+                You May Also Like
+        </h1>
+        <div className='w-full sm:px-12 lg:px-20 py-10 flex justify-center'>
+            <Carousel className='w-full max-w-[90%] bg-white lg:max-w-[900px] sm:max-w-[290px] md:max-w-[700px] xl:max-w-[1300px]'
+            opts={{
+                loop: true,
+            }}
+            >
+                <CarouselContent>
+                        {products.slice(0, 6).map((product, index) => (
+                            <CarouselItem key={index} className='flex flex-col md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
+                                <Image
+                                src={product.image}
+                                alt='product'
+                                width={1000}
+                                height={1000}
+                                className='w-[300px] h-[400px] '
+                                />
+                                <h2>
+                                    {product.name}
+                                </h2>
+                                <p>
+                                    PKR {product.price}
+                                </p>
+                            </CarouselItem>
+                        ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
+        </>
     )
 };
 
