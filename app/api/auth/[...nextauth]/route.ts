b2a48d972d9bb5c877a03111eab0e049692378e8
Tahasiraj1 <tahasiraj242@gmail.com>
2024-11-14 11:56:25 +0500

    twentyone

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions = {
+export const GET = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -10,11 +10,8 @@ export const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
-    signIn: '/auth/signin', // optional: custom sign-in page
+    signIn: '/auth/signin', // optional custom sign-in page
   },
-};
+});
 
-const handler = NextAuth(authOptions);
-
-export const GET = handler;
-export const POST = handler;
+export const POST = GET;
