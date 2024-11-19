50000edc3b4c8adb1b7f34f3056e643f7dc6f7a3
Tahasiraj1 <tahasiraj242@gmail.com>
2024-11-19 19:21:17 +0500

    deleted api folder because project has clerk now

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from 'next-auth';
-import { authOptions } from './authOptions'; // Import from a separate file
-
-const handler = NextAuth(authOptions);
-
-export const GET = handler;
-export const POST = handler;
-
-
