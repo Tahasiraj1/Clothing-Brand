d21829070581519fbbe4f837d60353e4770e3f7a
Tahasiraj1 <tahasiraj242@gmail.com>
2024-12-09 16:18:02 +0500

    Also removed useUser becuase it can only be used inside clerkProvider.

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from "react"
-import { useUser } from "@clerk/nextjs"
+// import { useUser } from "@clerk/nextjs"
 import { useRouter } from 'next/navigation'
 import { PackageSearch } from 'lucide-react'
 import {
@@ -53,23 +53,23 @@ export default function DashboardClient({ orders }: { orders: Order[] }) {
   const currentOrders = orders.slice(indexOfFirstOrder, indexOfLastOrder)
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
-  const { user, isLoaded } = useUser()
+  // const { user, isLoaded } = useUser()
   const router = useRouter()
 
-  if (!isLoaded) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
-      </div>
-    )
-  }
+  // if (!isLoaded) {
+  //   return (
+  //     <div className="flex items-center justify-center min-h-screen">
+  //       <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+  //     </div>
+  //   )
+  // }
 
-  const role = user?.publicMetadata?.role
+  // const role = user?.publicMetadata?.role
 
-  if (role !== 'admin') {
-    router.push('/')
-    return null
-  }
+  // if (role !== 'admin') {
+  //   router.push('/')
+  //   return null
+  // }
 
   if (!Array.isArray(orders)) {
     return (
