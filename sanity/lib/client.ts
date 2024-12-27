94589993be7911a9f584767adb186b7ab43c0770
Tahasiraj1 <tahasiraj242@gmail.com>
2024-12-27 23:00:08 +0500

    reverting to old code

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false, 
   token: process.env.SANITY_API_TOKEN,
 })
